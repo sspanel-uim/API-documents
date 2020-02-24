a20ada0f1e6705aeb51e36bd4d51e7ff8e4b76cf
laurieryayoi <14360417+laurieryayoi@users.noreply.github.com>
2020-02-25 03:12:48 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'SSpenl-Uim Api文档',
+    themeConfig: {
+        nav: [{
+            text: 'Home',
+            link: '/'
+        }],
+        sidebar: [{
+            title: '首页',
+            path: '/'
+        }]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+# SSpanel-Uim API文档
+
+git clone后安装依赖
+```
+yarn install
+```
+
+启动本地serve
+```
+yarn docs:dev
+```
+
+编译
+```
+yarn docs:build
+```
